ca443c3eaa8d66572c33d3f73d519e5bbb2a4845
alirrah <alir.rah81@gmail.com>
2023-03-15 04:52:40 +0330

    add bootstrap

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const btn = document.getElementById('add');
+const btnone = document.getElementById('Add');
 
 btn.addEventListener("click", () => {
     var name = document.getElementById("name");
-    var gender = document.getElementById("gender");
-    var designation = document.getElementById("designation");
-    var date = document.getElementById("date");
+    var gender = document.getElementById("teacher");
+    var designation = document.getElementById("time");
     var table = document.getElementById("tableData");
     var rowCount = table.rows.length;
     var row = table.insertRow(rowCount);
     row.insertCell(0).innerHTML= name.value;
     row.insertCell(1).innerHTML= gender.value;
     row.insertCell(2).innerHTML= designation.value;
-    row.insertCell(3).innerHTML= date.value;
+});
+
+btnone.addEventListener("click", () => {
+    var name = document.getElementById("nameDars");
+    var gender = document.getElementById("number");
+    var designation = document.getElementById("avg");
+    var table = document.getElementById("table");
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    row.insertCell(0).innerHTML= name.value;
+    row.insertCell(1).innerHTML= gender.value;
+    row.insertCell(2).innerHTML= designation.value;
 });
